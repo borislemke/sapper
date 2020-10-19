9f75fff77ccb45b785c268d595f9448042350747
Boris Lemke <dimaslemke@gmail.com>
2020-10-19 16:00:44 +0800

    add promo card

---
+++ src/components/PromoCard.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let promo
+</script>
+
+<style>
+    .wrapper {
+        background-color: white;
+        border-radius: 4px;
+        margin-bottom: 12px;
+        padding: 12px;
+    }
+
+    h2 {
+        font-size: 21px;
+        margin: 0 0 4px;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .outlet-name {
+        font-size: 12px;
+        font-weight: 600;
+        margin: 0 0 4px;
+        opacity: .54;
+    }
+</style>
+
+<div class="wrapper">
+    <p class="outlet-name">
+        {promo.outlet}
+    </p>
+    <h2>{promo.title}</h2>
+    <p>
+        {promo.description}
+    </p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,25 @@
 <script>
 	import SearchBar from "../components/SearchBar/SearchBar.svelte";
+	import PromoCard from "../components/PromoCard.svelte";
+
+	const promos = [
+		{
+			title: "20% Off",
+			description: "Get 20% off on all cocktails, 24/7",
+			outlet: "Starbucks Dewata"
+		},
+		{
+			title: "Buy 2 Get 1",
+			description: "On all appetizers and desserts",
+			outlet: "House of Waffles"
+		}
+	]
 </script>
 
 <style>
+	.list {
+		padding: 0 24px;
+	}
 </style>
 
 <svelte:head>
@@ -10,4 +27,9 @@
 </svelte:head>
 
 <SearchBar/>
-<h1>Ciplah</h1>
+
+<div class="list">
+	{#each promos as promo}
+	<PromoCard promo={promo}/>
+	{/each}
+</div>
